6a51ad9f597face1a326142afbebb72935c84d6a
thesaintzion <saintzion777@gmail.com>
2020-11-03 12:43:19 +0000

    map redux state  a component  prop with redux connect...

--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
-import img1 from '../assets/img/consult.jpg';
-import img2 from '../assets/img/bonus.jpg';
+// import img1 from '../assets/img/consult.jpg';
+// import img2 from '../assets/img/bonus.jpg';
 import img3 from '../assets/img/financial2.jpg';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { connect } from 'react-redux';
 import Modal from 'react-bootstrap/Modal';
 
-// import ListGroup from 'react-bootstrap/ListGroup';
-// import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
+// Map the redux state to the prop of a component
 
+const mapStateToProp = state => {
+    return { posts: state.posts };
+}
 
+const SinglePost = (prop) => {
+    console.log('The Post Length', prop.posts.length)
+   return ( prop.posts.length < 1 ? <p className="text-white">No found</p> : 
+   prop.posts.map(post => (
+    <div key={post.id} className="col-lg-4">
+      <Card className="shadow border-0">
+          <Card.Img variant="top" src={img3} />
+          <Card.Body>
+            <Card.Title>{post.title} {post.id}</Card.Title>
+              <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+              </Card.Text>
+              <Button variant="primary" className="w-100">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    ))  
+   )
+}
 
-
- class Posts extends Component {
+ class CPosts extends Component {
      
      state = {
          showModal: false,
@@ -35,85 +57,18 @@ import Modal from 'react-bootstrap/Modal';
     }
 
     render() {
+        console.log('The props', this.props)
         return (
             <>
-            {/* <Modal show={this.state.showModal} onHide={this.handleClose} >
-  <Modal.Header closeButton>
-    <Modal.Title>ADD NEW</Modal.Title>
-  </Modal.Header>
-  <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-  <Modal.Footer>
-    <Button variant="secondary" onClick={() => this.handleClose}>
-      Close
-    </Button>
-    <Button variant="primary" onClick={() => this.handleClose}>
-      Save Changes
-    </Button>
-  </Modal.Footer>
-</Modal> */}
-
-<div className="posts py-5 bg-dark">
+  
+            <div className="posts py-5 bg-dark">
                 <div className="container">
                 <h1 className="text-white text-center mb-4">POST</h1>
-
                 <div className="row">
-                    <div className="col-lg-4">
-                    <Card className="shadow border-0">
-                        <Card.Img variant="top" src={img3} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary" className="w-100">Go somewhere</Button>
-                          
-                        </Card.Body>
-                        </Card>
-                    </div>
-                    <div className="col-lg-4">
-                    <Card className=" shadow border-0">
-                        <Card.Img variant="top" src={img2} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary" className="w-100">Go somewhere</Button>
-                          
-                        </Card.Body>
-                        </Card>
-                    </div>
-
-                    <div className="col-lg-4">
-                    <Card className="shadow border-0">
-                        <Card.Img variant="top" src={img1} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary" className="w-100">Go somewhere</Button>
-                          
-                        </Card.Body>
-                        </Card>
-                    </div>
-
-                    {/* <div className="col-lg-12 text-center mt-5" onClick={() => this.handleShow}>
-                    <Button variant="light">ADD MORE POST +</Button>
-                    </div> */}
-
+                        <SinglePost posts={this.props.posts} />
                 </div>
-
-
- 
                 </div>
             </div>
-
-
-
 
 </>
         );
@@ -121,5 +76,7 @@ import Modal from 'react-bootstrap/Modal';
        
     }
 }
+
+const Posts = connect(mapStateToProp)(CPosts);
 
 export default Posts;
